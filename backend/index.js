37fddcc2cc4d8f0a33fee828fe4cb2bf921c8044
Haseeb <haseebjamal88@gmail.com>
2026-09-17 14:39:45 +0500

    configuring vercel file

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,30 @@
+import "dotenv/config";
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
+
 import "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
-dotenv.config();
+
 const app = express();
+
 const PORT = process.env.PORT || 5000;
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
+
 app.get("/", (req, res) => {
   res.json({
     success: true,
@@ -27,6 +32,10 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
